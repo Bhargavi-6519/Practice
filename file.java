9d63173d6b3b5a036d8b51760550ed13c3202555
Bhargavi-6519 <bhargavi6519@gmail.com>
2024-11-26 23:09:24 +0530

    new

--- file.java
+++ file.java
@@ -1,5 +1,7 @@
 public class file{
     public static void main(String[] args) {
-        System.out.println("Hello git!!!!");
+        int a=10;
+        int b=100;
+        System.out.println("Sum is: "+(a+b));
     }
 }
